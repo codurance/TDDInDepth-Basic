ff85afdc741eae0bfd25c47db22af2045e067cea
Benjamin Rae <benjaminrae93@gmail.com>
2024-07-05 14:23:14 +0200

    Module1: How we do TDD: TDD Cycle: Refactor Phase

--- src/main/java/com/codurance/module1/howWeDoTDD/threeRulesOfTDD/FizzBuzz.java
+++ src/main/java/com/codurance/module1/howWeDoTDD/threeRulesOfTDD/FizzBuzz.java
@@ -2,6 +2,18 @@ package com.codurance.module1.howWeDoTDD.threeRulesOfTDD;
 
 public class FizzBuzz {
     public String convert(int number) {
-        throw new UnsupportedOperationException("Method not implemented");
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "FizzBuzz";
+        }
+
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+
+        if (number % 5 == 0) {
+            return "Buzz";
+        }
+
+        return String.valueOf(number);
     }
 }
--- src/test/java/com/codurance/module1/howWeDoTDD/threeRulesOfTDD/FizzBuzzShould.java
+++ src/test/java/com/codurance/module1/howWeDoTDD/threeRulesOfTDD/FizzBuzzShould.java
@@ -2,7 +2,7 @@ package com.codurance.module1.howWeDoTDD.threeRulesOfTDD;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FizzBuzzShould {
     @Test
@@ -11,6 +11,106 @@ class FizzBuzzShould {
         FizzBuzz fizzBuzz = new FizzBuzz();
 
         String actual = fizzBuzz.convert(1);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void convert2To2() {
+        String expected = "2";
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String actual = fizzBuzz.convert(2);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void convert3ToFizz() {
+        String expected = "Fizz";
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String actual = fizzBuzz.convert(3);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void convert6ToFizz() {
+        String expected = "Fizz";
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String actual = fizzBuzz.convert(6);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void convert9ToFizz() {
+        String expected = "Fizz";
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String actual = fizzBuzz.convert(9);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void convert5ToBuzz() {
+        String expected = "Buzz";
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String actual = fizzBuzz.convert(5);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void convert10ToBuzz() {
+        String expected = "Buzz";
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String actual = fizzBuzz.convert(10);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void convert20ToBuzz() {
+        String expected = "Buzz";
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String actual = fizzBuzz.convert(20);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void convert15ToFizzBuzz() {
+        String expected = "FizzBuzz";
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String actual = fizzBuzz.convert(15);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void convert30ToFizzBuzz() {
+        String expected = "FizzBuzz";
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String actual = fizzBuzz.convert(30);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void convert45ToFizzBuzz() {
+        String expected = "FizzBuzz";
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String actual = fizzBuzz.convert(45);
 
         assertEquals(expected, actual);
     }
