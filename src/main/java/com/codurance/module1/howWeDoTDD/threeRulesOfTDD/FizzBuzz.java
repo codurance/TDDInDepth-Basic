7fe0e494a5727802e6e6a558f3aee0da2396c0e9
Benjamin Rae <benjaminrae93@gmail.com>
2024-07-05 10:10:32 +0200

    Module1: How we do TDD: Three rules of TDD: Break rule 2

--- src/main/java/com/codurance/module1/howWeDoTDD/threeRulesOfTDD/FizzBuzz.java
+++ src/main/java/com/codurance/module1/howWeDoTDD/threeRulesOfTDD/FizzBuzz.java
@@ -2,8 +2,16 @@ package com.codurance.module1.howWeDoTDD.threeRulesOfTDD;
 
 public class FizzBuzz {
     public String convert(int number) {
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "FizzBuzz";
+        }
+        
         if (number % 3 == 0) {
             return "Fizz";
+        }
+
+        if (number % 5 == 0) {
+            return "Buzz";
         }
 
         return String.valueOf(number);
