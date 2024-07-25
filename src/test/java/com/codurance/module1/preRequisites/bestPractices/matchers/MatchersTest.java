bc2fb3a7693869b681814979dca16bffdf3c28db
Koldo Picaza <koldo.picaza@codurance.com>
2024-07-25 10:39:07 +0200

    feat(matchers): add examples uf improved usage of junit matchers

--- src/test/java/com/codurance/module1/preRequisites/bestPractices/matchers/MatchersTest.java
+++ src/test/java/com/codurance/module1/preRequisites/bestPractices/matchers/MatchersTest.java
@@ -4,11 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.testcontainers.shaded.org.hamcrest.MatcherAssert.assertThat;
 
 public class MatchersTest {
     @Test
@@ -16,23 +15,61 @@ public class MatchersTest {
         int a = 1;
         int b = 1;
 
+        // Bad Usage
         assertTrue(a == b);
+
+        // Improved Usage
+        assertEquals(a, b);
     }
 
     @Test
     public void shouldCheckForANull() {
         String a = null;
 
+        // Bad Usage
         assertEquals(null, a);
+
+        // Improved Usage
+        assertNull(a);
     }
 
     @Test
     public void shouldCheckForAnException() {
+        // Bad Usage
         try {
             somethingWillFail();
         } catch (Exception e) {
             assertEquals("Something went wrong :-S", e.getMessage());
         }
+
+        // Improved Usage
+        assertThrows(Exception.class, () -> somethingWillFail());
+    }
+
+    @Test
+    public void shouldCheckForBooleanConditions() {
+        // Bad Usage
+        assertTrue(isValid());
+
+        boolean a = isValid();
+        // Improved Usage
+        assertNotNull(a);
+        assertTrue(a);
+    }
+
+    @Test
+    public void shouldCheckForComplexConditions() {
+
+        String[] array = {"Test", "Test1", "Test2"};
+        List<String> collection = new ArrayList<>(Arrays.asList(array));
+        boolean condition = collection != null && collection.size() > 0;
+
+        // Bad Usage
+        assertTrue(condition);
+
+        // Improved Usage
+        assertNotNull(collection);
+        assertSame(3, collection.size());
     }
 
     @Test
@@ -41,19 +78,10 @@ public class MatchersTest {
         String[] array = {"Test", "Test1", "Test2"};
         List<String> collection = new ArrayList<>(Arrays.asList(array));
 
+        // Bad Usage
         assertTrue(collection.contains(a));
-    }
 
-    @Test
-    public void shouldCheckForBooleanConditions() {
-        assertTrue(isValid());
-    }
-
-    @Test
-    public void shouldCheckForComplexConditions() {
-        String[] array = {"Test", "Test1", "Test2"};
-        List<String> collection = new ArrayList<>(Arrays.asList(array));
-        assertTrue(collection != null && collection.size() > 0);
+        // Improved Usage: We will resolve in the next video with AssertJ
     }
 
     private boolean isValid() {
