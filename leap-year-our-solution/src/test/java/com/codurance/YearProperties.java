5ed09a69580b2899f9f7726c2c2eaad408c3994e
Benjamin Rae <benjaminrae93@gmail.com>
2024-07-29 15:36:45 +0200

    test(leap-year): create property test for years divisible by 4

--- leap-year-our-solution/src/test/java/com/codurance/YearProperties.java
+++ leap-year-our-solution/src/test/java/com/codurance/YearProperties.java
@@ -4,8 +4,7 @@ import net.jqwik.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class YearProperties
-{
+public class YearProperties {
     @Property
     public void yearsDivisibleBy400AreLeapYears(@ForAll("yearsDivisibleBy400") int yearDivisibleBy400) {
         Year year = new Year(yearDivisibleBy400);
@@ -15,8 +14,22 @@ public class YearProperties
         assertTrue(isLeapYear);
     }
 
+    @Property
+    public void yearsDivisibleBy4AreLeapYears(@ForAll("yearsDivisibleBy4") int yearDivisibleBy4) {
+        Year year = new Year(yearDivisibleBy4);
+
+        boolean isLeapYear = year.isLeap();
+
+        assertTrue(isLeapYear);
+    }
+
     @Provide
     private Arbitrary<Integer> yearsDivisibleBy400() {
         return Arbitraries.integers().between(1, 100).map(year -> year * 400);
+    }
+
+    @Provide
+    private Arbitrary<Integer> yearsDivisibleBy4() {
+        return Arbitraries.integers().between(1, 99).map(year -> year * 4);
     }
 }
